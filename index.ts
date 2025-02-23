7ff1ddeef749d269f94f0e92cd91873485225b08
fierillo <fierillo@protonmail.com>
2025-02-22 23:28:42 -0300

    1.0.0: verduguillo is alive in Github

---
+++ index.ts
@@ -0,0 +1,86 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+import http from 'http';
+
+// Load environment variables from .env file
+config();
+
+// Create a simple server to keep the bot alive
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Verduguillo is alive!');
+}).listen(3000);
+
+// Define main variables
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMessageReactions
+    ]
+});
+// Load Discord token
+client.login(process.env.DISCORD_TOKEN);
+const emojiName = '💩'; 
+const reactionThreshold = 1; 
+const shitcoinerRoleName = 'shitcoiner'; 
+
+// Defines punishment function
+async function getPunishment(member: any, shitcoinerRole: any, targetUser: any, channel: any) {
+    // Checks if the member exists
+    if (!member) {
+        console.log(`Miembro no encontrado para el usuario: ${targetUser.tag}`);
+        return;
+    }
+    // Checks if the shitcoiner role exists
+    if (!shitcoinerRole) {
+        console.log(`"${shitcoinerRoleName}" role doesn't exist in the server`);
+        return;
+    }
+
+    // Execute punishment, only if the user doesn't have the shitcoiner role
+    if (!member.roles.cache.has(shitcoinerRole.id)) {
+        try {
+            await member.roles.add(shitcoinerRole);
+            await channel.send(`El usuario ${targetUser.tag} fue castigado por acumulacion de caquitas 💩`);
+            console.log(`User ${targetUser.tag} was punished successfully`);
+        } catch (error) {
+            console.error(`Error trying to punish user ${targetUser.tag}`, error);
+        }
+    } else {
+        console.log(`User ${targetUser.tag} already was punished!`);
+    }
+}
+
+// Starting message, load old messages in the cache
+client.on('ready', async () => {
+    console.log(`Bot listo como ${client.user?.tag}`);
+});
+
+client.on('messageReactionAdd', async (reaction) => {
+    console.log(`New reaction detected, analyzing...`);
+    if (reaction.partial) await reaction.fetch();
+    if (reaction.message.partial) await reaction.message.fetch();
+
+    // Ignore bot reactions
+    if (!reaction.message.author || reaction.message.author.bot) return;
+
+    // Verify if the reaction is :poop:
+    if (reaction.emoji.name === emojiName) {
+        console.log(`Shit detected for user: ${reaction.message.author.tag}`);
+        // Get the reaction count
+        const reactionCount = reaction.count;
+
+        // If message has more than threshold reactions, punish the author
+        if (reactionCount ?? 0 >= reactionThreshold) {
+            const guild = reaction.message.guild;
+            if (!guild) return;
+            const targetUser = reaction.message.author; // Message author
+            const member = guild.members.cache.get(targetUser.id);
+            const shitcoinerRole = guild.roles.cache.find(role => role.name === shitcoinerRoleName);
+            
+            const channel = reaction.message.channel;
+            getPunishment(member, shitcoinerRole, targetUser, channel);
+        }
+    }
+});
